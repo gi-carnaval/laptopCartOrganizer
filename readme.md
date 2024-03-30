7dbda76e218d819d5b77749b6c3b3105b60cd7df
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-03-30 00:52:42 -0300

    Correction of laptops codes and restyling the mobile design

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Sistema de Gerenciamento de Notebooks Escolares
+
+## Descrição
+O Sistema de Organização de Notebooks Escolares é uma aplicação web desenvolvida para facilitar a organização e identificação dos notebooks utilizados pelos alunos em uma escola. O sistema utiliza códigos QR estampados nos notebooks para identificar a qual carrinho móvel eles pertencem.
+
+## Funcionalidades
+- Leitura de Códigos QR: O sistema é capaz de ler os códigos QR estampados nos notebooks para identificar sua localização.
+- Associação com Carrinhos Móveis: Cada notebook está associado a um carrinho móvel específico, permitindo uma organização eficiente.
+- Interface Web Intuitiva: A interface do sistema é amigável e de fácil utilização, tornando o processo de organização mais simples para os usuários.
+
+## Instalação e Uso
+### Requisitos
+- Node.js
+- NPM (Node Package Manager)
+
+## Instalação
+- Clone o repositório do projeto: git clone https://github.com/seu-usuario/sistema-organizacao-notebooks.git
+- Acesse o diretório do projeto: cd sistema-organizacao-notebooks
+- Instale as dependências do projeto: npm install
+- Inicie o servidor: npm run dev
+- Acesse o sistema através do navegador: http://localhost:3000
+
+## Licença
+Este projeto está licenciado sob a Licença MIT.
--- src/main.ts
+++ src/main.ts
@@ -23,35 +23,47 @@ let resultWaiting = ''
 
 const carts: Cart = {
   carrinho_1: [
-    1265999, 1266046, 1266045, 1266043, 1265994, 1266009, 
-    1265976, 1266052, 1266033, 1265979, 1266039, 1266053, 
-    1266012, 1266022, 1265971,1265981,1266047,1266004
+    1265999, 1265976, 1266012, 1266046, 1266052, 1266022,
+    1266045, 1266033, 1265971, 1266043, 1265979, 1265981,
+    1265994, 1266039, 1266047, 1266009, 1266053, 1266004
+
   ],
-  carrinho_2: [],
+  carrinho_2: [
+    1266003, 1265998, 1266023, 1266044, 1266042, 1265977,
+    1265990, 1265985, 1266048, 1266031, 1265974, 1266036,
+    1266029, 1266013, 1266007, 1265975, 1266030
+
+  ],
   carrinho_3: [],
   carrinho_4: [
-    1266050,1266036,1266017,1265982,1266037,1266056,1265991,
-    1265980,1265978,1266034,1266015,1266000,1266001,1266010,
-    1266057,1266038,1266021,1265993,1266025,1266018,1266055,
-    1265989,1266054,1265987,1265984
+    1266050, 1265980, 1266057, 1265989,
+    1265978, 1266038, 1266054, 1266017,
+    1266034, 1266021, 1265987, 1265982,
+    1266015, 1265993, 1265984, 1266037,
+    1266000, 1266025, 1266056, 1266001,
+    1266018, 1265991, 1266010, 1266055
+
   ]
 }
 
 const carrinhosDicionario: CarrinhosDicionarioProps = {
-  carrinho_1: "CARRINHO 1", carrinho_2: "CARRINHO 2", carrinho_3: "CARRINHO 3",
+  carrinho_1: "CARRINHO 1",
+  carrinho_2: "CARRINHO 2",
+  carrinho_3: "CARRINHO 3",
   carrinho_4: "CARRINHO 4"
 }
 
-function verifyCart(code: string): string | null {
+function verifyCart(code: string) {
   cartNumberResult.innerHTML = ""
   cartNumberResult.style.background = "transparent"
   for (const cartKey in carts) {
     const cartItems = carts[cartKey];
     if (cartItems.includes(Number(code))) {
       resultSpan.innerHTML = `O notebook ${code} pertence ao`
+
       cartNumberResult.innerHTML = carrinhosDicionario[cartKey]
       cartNumberResult.style.background = "red"
-      return cartKey;
+
     } else {
       resultSpan.innerHTML = `O notebook ${code} não pertence a nenhum carrinho`
     }
@@ -83,9 +95,9 @@ scanner.setInversionMode("both");
 
 camList.addEventListener('change', (event) => {
   const target = event.target as HTMLSelectElement;
-    if (target && target instanceof HTMLSelectElement) {
-        scanner.setCamera(target.value);
-    }
+  if (target && target instanceof HTMLSelectElement) {
+    scanner.setCamera(target.value);
+  }
 });
 
 const botaoIniciar = document.getElementById('start-button')

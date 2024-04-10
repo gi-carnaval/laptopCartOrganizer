5f6bb47c7fe256a3f5f39b2114cec1309e59b69d
Giovani Andrino Carnaval <gi.carnaval91@gmail.com>
2024-04-10 15:00:41 -0300

    Update laptop codes

--- src/main.ts
+++ src/main.ts
@@ -7,77 +7,84 @@ const camList = <HTMLElement>document.getElementById('cam-list');
 type CartKey = string;
 type Cart = Record<CartKey, number[]>;
 type CarrinhosDicionarioProps = {
-  [key: string]: string;
+    [key: string]: string;
 };
 
 interface ResultItemProps {
-  data: string;
-  cornerPoints: {
-    x: number;
-    y: number;
-  }[];
+    data: string;
+    cornerPoints: {
+        x: number;
+        y: number;
+    }[];
 }
+
 let resultSpan = <HTMLElement>document.getElementById('resultSpan');
 let cartNumberResult = <HTMLElement>document.getElementById('cartNumberResult');
 let resultWaiting = ''
 
 const carts: Cart = {
-  carrinho_1: [
-    1265999, 1265976, 1266012, 1266046, 1266052, 1266022,
-    1266045, 1266033, 1265971, 1266043, 1265979, 1265981,
-    1265994, 1266039, 1266047, 1266009, 1266053, 1266004
+    carrinho_1: [
+        1265977, 1266013, 1266048, 1266035, 1265974,
+        1266018, 1265999, 1266021, 1266052, 1265993,
+        1266055, 1265987, 1266017, 1266056, 1266034,
+        1265984, 1266000
 
-  ],
-  carrinho_2: [
-    1266003, 1265998, 1266023, 1266044, 1266042, 1265977,
-    1265990, 1265985, 1266048, 1266031, 1265974, 1266036,
-    1266029, 1266013, 1266007, 1265975, 1266030
+    ],
+    carrinho_2: [
+        1266040, 1265985, 1266023, 1266049, 1266050, 
+        1266036, 1265975, 1266005, 1265994, 1266047, 
+        1266044, 1266008, 1266028, 1265998, 1266003, 
+        1265971, 1266030,
 
-  ],
-  carrinho_3: [],
-  carrinho_4: [
-    1266050, 1265980, 1266057, 1265989,
-    1265978, 1266038, 1266054, 1266017,
-    1266034, 1266021, 1265987, 1265982,
-    1266015, 1265993, 1265984, 1266037,
-    1266000, 1266025, 1266056, 1266001,
-    1266018, 1265991, 1266010, 1266055
-
-  ]
+    ],
+    carrinho_3: [
+        1266006, 1266041, 1266014, 1266031, 1266020,
+        1266029, 1266042, 1266051, 1266019, 1266002,
+        1265970, 1265983, 1266011, 1266004, 1266024,
+        1266058, 1266007, 
+    ],
+    carrinho_4: [
+        1265976, 1265977, 1265978, 1265979, 1265980,
+        1265981, 1265982, 1265983, 1265984, 1265985,
+        1265986, 1265987, 1265988, 1265989, 1265990,
+        1265991, 1265992, 1265993, 1265994, 1265995,
+        1265996, 1265997, 
+    ]
 }
 
 const carrinhosDicionario: CarrinhosDicionarioProps = {
-  carrinho_1: "CARRINHO 1",
-  carrinho_2: "CARRINHO 2",
-  carrinho_3: "CARRINHO 3",
-  carrinho_4: "CARRINHO 4"
+    carrinho_1: "CARRINHO 1",
+    carrinho_2: "CARRINHO 2",
+    carrinho_3: "CARRINHO 3",
+    carrinho_4: "CARRINHO 4"
 }
 
 function verifyCart(code: string) {
-  cartNumberResult.innerHTML = ""
-  cartNumberResult.style.background = "transparent"
-  for (const cartKey in carts) {
-    const cartItems = carts[cartKey];
-    if (cartItems.includes(Number(code))) {
-      resultSpan.innerHTML = `O notebook ${code} pertence ao`
+    cartNumberResult.innerHTML = ""
+    cartNumberResult.style.background = "transparent"
 
-      cartNumberResult.innerHTML = carrinhosDicionario[cartKey]
-      cartNumberResult.style.background = "red"
+    for (const cartKey in carts) {
+        const cartItems = carts[cartKey];
 
-      return
+        if (cartItems.includes(Number(code))) {
+            resultSpan.innerHTML = `O notebook ${code} pertence ao`
+            cartNumberResult.innerHTML = carrinhosDicionario[cartKey]
+            cartNumberResult.style.background = "red"
 
-    } else {
-      resultSpan.innerHTML = `O notebook ${code} não pertence a nenhum carrinho`
-      cartNumberResult.innerHTML = ""
-      cartNumberResult.style.background = "transparent"
-    }
+            return
+
+        } else {
+            resultSpan.innerHTML = `O notebook ${code} não pertence a nenhum carrinho`
+            cartNumberResult.innerHTML = ""
+            cartNumberResult.style.background = "transparent"
+        }
   }
 
-  return null;
+    return null;
 }
 
 export function onScanSuccess(decodedText: string) {
-  verifyCart(decodedText.substring(0, 7));
+    verifyCart(decodedText.substring(0, 7));
 }
 
 function setResult(result: ResultItemProps) {
@@ -89,8 +96,8 @@ function setResult(result: ResultItemProps) {
 }
 
 const scanner = new QrScanner(video, result => setResult(result), {
-  highlightScanRegion: true,
-  highlightCodeOutline: true,
+    highlightScanRegion: true,
+    highlightCodeOutline: true,
 });
 
 videoContainer.className = "example-style-1"
@@ -99,20 +106,21 @@ scanner.setInversionMode("both");
 
 camList.addEventListener('change', (event) => {
   const target = event.target as HTMLSelectElement;
-  if (target && target instanceof HTMLSelectElement) {
-    scanner.setCamera(target.value);
-  }
+    if (target && target instanceof HTMLSelectElement) {
+        scanner.setCamera(target.value);
+    }
 });
 
 const botaoIniciar = document.getElementById('start-button')
 const botaoEncerrar = document.getElementById('stop-button')
 
 if (botaoIniciar && botaoEncerrar) {
-  botaoIniciar.addEventListener('click', () => {
-    scanner.start();
-  });
+    
+    botaoIniciar.addEventListener('click', () => {
+        scanner.start();
+    });
 
-  botaoEncerrar.addEventListener('click', () => {
-    scanner.stop();
+    botaoEncerrar.addEventListener('click', () => {
+        scanner.stop();
   });
 }
